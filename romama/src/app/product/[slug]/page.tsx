826aadb6b228de5263e6b2444c24504d48750feb
IScetta <150315667+IScetta@users.noreply.github.com>
2024-10-01 20:46:24 -0300

    v0.8

--- romama/src/app/product/[slug]/page.tsx
+++ romama/src/app/product/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import ProductImages from "@/components/ProductPage/ProductImages";
 
-export default function Product(){
-    
+interface Params {
+    slug: string;
+  }
+
+export default function Product({params}:{params:Params}){
+    const { slug } = params;
+    console.log(slug)
     return(
         <div className="mt-24 mx-4">
             <ProductImages/>
